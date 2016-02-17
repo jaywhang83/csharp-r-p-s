07c6b84d588f7f00308b2c31914421940aeb3313
Michael and Jay <student@epicodus.com>
2016-02-17 12:57:24 -0800

    add css and fixed radio button

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -1,7 +1,8 @@
 using Nancy;
+using System;
 using System.Collections.Generic;
 
-namespace AnagramNameSpace
+namespace RPSNS
 {
   public class HomeModule : NancyModule
   {
@@ -9,6 +10,16 @@ namespace AnagramNameSpace
     {
       Get["/"] = _ => View["index.cshtml"];
 
+      Post["/result"] = _ => {
+        
+        string p1 = Request.Form["p1"];
+        string p2 = Request.Form["p2"];
+        Console.WriteLine(p1);
+        Console.WriteLine(p2);
+        RockPaperScissors newRPS = new RockPaperScissors();
+        return View["result.cshtml",  newRPS.Play(p1, p2)];
+
+      };
     }
   }
 }
